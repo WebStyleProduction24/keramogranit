473ac019e3385090d629b9b89d0ad17f176ff327
Jack36VRN <36456429+Jack36VRN@users.noreply.github.com>
2019-12-10 12:22:15 +0300

    php

--- footer.php
+++ footer.php
@@ -1,9 +1,13 @@
 
-<footer class="footer">
-	<p>2019, Универдом</p>
-</footer>
+	<footer>
+		<p>2019, Универдом</p>
+	</footer>
 
 
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<script src="js/formScript.js"></script>
+	<script src="js/main.js"></script>
 </body>
 
 </html>

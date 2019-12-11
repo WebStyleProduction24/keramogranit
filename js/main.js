29658d3d974c0909852465f3ef04965fcf3e962c
Jack36VRN <36456429+Jack36VRN@users.noreply.github.com>
2019-12-11 12:50:32 +0300

    Revert "Merge branch 'Korolev' of https://github.com/WebStyleProduction24/keramogranit into Korolev"
    This reverts commit a21a9b840642ab6dc19972aa4066f9e6cf1c795c, reversing
    changes made to 087b5f25c8e5951c460ebd227ac70eeb42e413a0.

--- js/main.js
+++ js/main.js
@@ -64,14 +64,6 @@ $(function(){
         });
 
     });
-
-    $('#open__modal').on('click', function() {
-        $('#close__modal').slideToggle(100, function(){
-            if( $(this).css('display') === "none"){
-                $(this).attr('display',"flex");
-            }
-        });
-    });
     let open__area=0;
     let close__area=["img/arrowUp.png","img/arrowDown.png"];
     $(this).find('.svg__arrow-area').click(function(){
@@ -100,49 +92,8 @@ $(function(){
         $(this).find('img').attr("src",close__style[open__style]);
     });
 
-    $('#thumbs .thumb a').each(function(i) {
-        $(this).addClass( 'itm'+i );
-        $(this).click(function() {
-            $('#images').trigger( 'slideTo', [i, 0, true] );
-            return false;
-        });
-    });
-    $('#thumbs a.itm0').addClass( 'selected' );
-
-    $('#images').carouFredSel({
-        direction: 'left',
-        circular: true,
-        infinite: false,
-        items: 1,
-        auto: false,
-        scroll: {
-            fx: 'directscroll',
-            onBefore: function() {
-                var pos = $(this).triggerHandler( 'currentPosition' );
-                $('#thumbs a').removeClass( 'selected' );
-                $('#thumbs a.itm'+pos).addClass( 'selected' );
-
-                var page = Math.floor( pos / 3 );
-                $('#thumbs').trigger( 'slideToPage', page );
-            }
-        }
-    });
-    $('#thumbs').carouFredSel({
-        direction: 'left',
-        circular: true,
-        infinite: false,
-        items: 3,
-        align: false,
-        auto: false,
-        height: 80,
-        margin: 0,
-        prev: '#prev',
-        next: '#next'
-    });
-
-
-
 
 
 });
+
 

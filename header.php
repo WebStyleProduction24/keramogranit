087b5f25c8e5951c460ebd227ac70eeb42e413a0
Ivan <57195819+JohnK311@users.noreply.github.com>
2019-12-11 12:21:49 +0300

    Адаптивная верстка 11.12.2019
    Добавление иконок под мобильную верстку, изменене хедера, добавление медиазапросов под размеры 1200 и 992 px

--- header.php
+++ header.php
@@ -8,6 +8,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/media.css">
 </head>
 
 <body>
@@ -16,14 +17,32 @@
 
 	<header>
 		<div class="contacts">
-			<div class="email"><svg width="16" height="12" viewBox="0 0 16 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-					<path fill-rule="evenodd" clip-rule="evenodd" d="M2 0C0.895431 0 0 0.895431 0 2V10C0 11.1046 0.895431 12 2 12H14C15.1046 12 16 11.1046 16 10V2C16 0.895431 15.1046 0 14 0H2ZM2.27735 2.58397C2.04759 2.4308 1.73715 2.49289 1.58397 2.72265C1.4308 2.95241 1.49289 3.26285 1.72265 3.41603L7.72265 7.41603L8 7.60093L8.27735 7.41603L14.2774 3.41603C14.5071 3.26285 14.5692 2.95241 14.416 2.72265C14.2628 2.49289 13.9524 2.4308 13.7226 2.58397L8 6.39907L2.27735 2.58397Z" fill="#FFDB4D" />
-				</svg>
-				<span>zakaz@keramogranit.ru</span></div>
-			<div class="phones"><svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-					<path d="M9.75372 5.17357L11.7237 0.888916L13.5213 1.62765C14.1779 1.86569 15.4026 2.785 15.048 4.55796C14.6934 6.33092 13.1601 8.64562 12.4378 9.58135L11.4528 10.7387C10.5663 11.6744 10.0738 12.0438 9.58135 12.4378C8.64562 13.1601 6.33092 14.6934 4.55796 15.048C2.785 15.4026 1.86569 14.1779 1.62765 13.5213L0.888916 11.7237L5.17357 9.75372L5.96155 11.4774C6.50516 11.3233 7.85075 10.6558 9.26109 9.26109C10.6558 7.85075 11.3233 6.50516 11.4774 5.96155L9.75372 5.17357Z" fill="#FFDB4D" />
-				</svg>
-				<span>+7 (495) 22-55-700</span><span>+7 (800) 000-00-00</span></div>
+
+			<div class="email mobilePhoneMail">
+				<a href=""><img src="img/MailYellowDesktop.png" alt=""></a>
+
+				<a href="#"><img src="img/PhoneYellowMobile.png" alt="" id="phoneMobile"></a>
+				<a href="#"><img src="img/MailYellowMobile.png" alt="" id='mailMobile'></a>
+
+				<span>zakaz@keramogranit.ru</span>
+
+			</div>
+
+
+			<div class="phones mobileFavouritesBusket">
+
+				<img src="img/PhoneYellowDesktop.png" alt="">
+				
+				
+				<a href="#"><img src="img/FavouriteYellowMobile.png" alt="" id="favouritesMobile"></a>
+				
+				<a href="#"><img src="img/BucketYellowMobile.png" alt="" id="busketMobile"></a>
+				
+				
+				
+				<a href="#"><span>+7 (495) 22-55-700</span></a><a href="#"><span>+7 (800) 000-00-00</span></a>
+
+			</div>
 		</div>
 		<div class="mainHeader">
 			<div class="logo"><a href="index.html"><img src="img/logo.png" alt=""></a></div>

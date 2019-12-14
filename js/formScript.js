93e59157a356a4b1e1344a3d3714b00c3e3466a4
Ivan <57195819+JohnK311@users.noreply.github.com>
2019-12-14 09:40:03 +0300

    14.12.2019
    Мелкие изменения

--- js/formScript.js
+++ js/formScript.js
@@ -1,10 +1,17 @@
 
 $(document).ready(function () {
-	$('.popup').hide();
+	$('.popup, .mail-window').hide();
 	$('#feedback').click(function () {
 	$('.popup').slideToggle(500);
 	});
 	$('#close').click(function(){
 		$('.popup').slideUp(500);
 	});
+	$('#mailMobile').click(function(){
+		$('.mail-window').slideToggle(500);
+	});
+	$('#mail-window-close').click(function(){
+		$('.mail-window').slideUp(500);
+	});
+	
 });

5d7d52d798483cad630aa81d4049e817cf3051a1
Saiferin <Saiferin@users.noreply.github.com>
2019-12-14 13:52:04 +0300

    update
    дописан js для карточки

--- js/main.js
+++ js/main.js
@@ -150,11 +150,15 @@ $(function(){
         hv = document.getElementById('show');
 
     bl.onmouseover = function () {
-        hv.style.display = 'block';
+        bl.style.display = 'none';
+        hv.style.display='block';
+
+
     }
 
-    hv.onmouseout = function () {
+    hv.onpointerleave = function () {
         hv.style.display = 'none';
+        bl.style.display = 'block';
     }
 
 

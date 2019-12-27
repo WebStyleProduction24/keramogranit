4509e14b56c26bdc30f6225375c181c13aaec2ec
Aizil <mr.badikov@bk.ru>
2019-12-28 00:57:06 +0300

    Обновление
    1.Переделал ховер, теперь он без js
    2. Выровнил на 320 карточку товара
    3. Отключил слайдер

--- hover-product.php
+++ hover-product.php
@@ -1,18 +1,47 @@
-<div class="product_icon test" id="hover">
-	<img src="<?php echo get_template_directory_uri();?>/img/Rectangle%20104.png" alt="">
-	<p>Керамогранит Estima Standard ST 07 матовый 300х300 мм io</p>
-	<div class="product_icon_price">
-		<span>650р/м<sup>2</sup></span>
-		<div class="product_icon_price_icons_block">
-			<div class="iconBusket"></div>
-			<div class="iconHeart"></div>
+<div class="product_icon" >
+	<div class="product_icon-flex d-flex">
+		
+		<div class="product_icon-img">
+			<button class="icon-btn shadow__btn"><img src="<?php echo get_template_directory_uri();?>/img/arrowUp.png" class="product_icon__img"></button>
+			<ul class="thumbs__item" id="thumbsS">		
+						<li><a href="<?php echo get_template_directory_uri();?>/img/slide1.png" title="Image 1"><img src="<?php echo get_template_directory_uri();?>/img/slide1.png" ></a></li>
+						<li><a href="<?php echo get_template_directory_uri();?>/img/slide2.png" title="Image 2"><img src="<?php echo get_template_directory_uri();?>/img/slide2.png"></a></li>
+						<li><a href="<?php echo get_template_directory_uri();?>/img/slide3.png" title="Image 3"><img src="<?php echo get_template_directory_uri();?>/img/slide3.png"></a></li>
+						<li><a href="<?php echo get_template_directory_uri();?>/img/slide4.png" title="Image 3"><img src="<?php echo get_template_directory_uri();?>/img/slide4.png"></a></li>				   
+					</ul>
+			<button class="icon-btn top__img"><img src="<?php echo get_template_directory_uri();?>/img/arrowDown.png" alt=""></button>
+		</div>
+		<div class="product_icon-item">
+			<div class="slider">
+				<p><img id="bigImg" src="<?php echo get_template_directory_uri();?>/img/slide1.png" ></p>
+			</div>  
+			<p class="product_icon-text" onclick="$('#exampleModal1').arcticmodal();">Керамогранит Estima Standard ST 07 матовый 300х300 мм io</p>
+			<div class="product_icon_price ">
+				<span class="font__bold d-flex">650р/ <span class="m2">м<sup>2</sup></span> </span>
+				<div class="product_icon_price_icons_block left">
+					<div class="iconBusket hidden-busket"></div>
+					<div class="iconHeart"></div>
+					<div class="iconBusket show-busket"></div>
+				</div>
+			</div>
+			<form method='post' action='#' class="form__item">
+				<div class="quantity left-top">
+					<input type='button' value='-' class='minus' field='quantity' />
+					<input type='text' name='quantity' value='0' class='qty' />
+					<input type='button' value='+' class='plus' field='quantity' />
+				</div>
+				<p class="quantity__text">Коробки</p>
+			</form>
+			<div class="hover__line"></div>
+
+
+			<div class="art">
+				<p>Артикул <span>119608</span></p>
+			</div>
 		</div>
 	</div>
-	<hr>
-	<div class="art">
-		<p>Артикул <span>119608</span></p>
-	</div>
 </div>
+<!--
 <div class="product_icon-slide " id="show" >
 	<div class="product_icon-flex d-flex">
 		
@@ -54,7 +83,7 @@
 			</div>
 		</div>
 	</div>
-	
+-->
 </div>
 <div class="product_icon-slide-rigth" >
 	<div class="product_icon-flex-rigth d-flex">
--- product-star.php
+++ product-star.php
@@ -1,5 +1,5 @@
 <div class="product_icon">
-	<img src="<?php echo get_template_directory_uri();?>/img/Rectangle%20104.png" alt="">
+	<img src="<?php echo get_template_directory_uri();?>/img/Rectangle%20104.png" class="product_icon__img">
 	<p>Керамогранит Estima Standard ST 07 матовый 300х300 мм io</p>
 	<div class="product_icon_price"><span>650р/м<sup>2</sup></span>
 		<div class="product_icon_price_icons_block">

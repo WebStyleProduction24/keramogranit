58dcfa233c07f631d6741341cc541c3ba90cd06f
Jack36VRN <36456429+Jack36VRN@users.noreply.github.com>
2020-01-08 23:15:32 +0300

    Update 2020/01/08 23:15 +3GMT
    Добавлен Хук вывода наименования товара в плитке

--- functions.php
+++ functions.php
@@ -59,14 +59,25 @@ define('WOOCOMMERCE_USE_CSS', false);
 remove_action('woocommerce_before_shop_loop_item_title', 'woocommerce_template_loop_product_thumbnail', 10);
 add_action('woocommerce_before_shop_loop_item_title', 'woo_product_loop_thumbnail', 10);
 
+remove_action('woocommerce_shop_loop_item_title', 'woocommerce_template_loop_product_title', 10);
+add_action('woocommerce_shop_loop_item_title', 'woo_product_loop_title', 10);
+
+
+
 //Новые функции для Хуков
 
-function woo_product_loop_thumbnail( $post = null ) {
 
+//Хук вывода миниатюры в плитке товаров
+function woo_product_loop_thumbnail( $post = null ) {
 	$src = get_the_post_thumbnail_url( $post, 'product-thumbnail' );
 	$alt = get_the_title();
-	$img = '<img src="' . $src . '" alt="'. $alt .'">';
-
-
+	$img = '<img src="' . $src . '" alt="' . $alt . '">';
 	echo $img;
+}
+
+//Хук вывода наименования в плитке товаров
+function woo_product_loop_title() {
+	$title = get_the_title();
+	$p = '<p>' . $title . '</p>';
+	echo $p;
 }
--- woocommerce/content-product.php
+++ woocommerce/content-product.php
@@ -57,9 +57,9 @@ if ( empty( $product ) || ! $product->is_visible() ) {
 	/**
 	 * Hook: woocommerce_shop_loop_item_title.
 	 *
-	 * @hooked woocommerce_template_loop_product_title - 10
+	 * @hooked woo_product_loop_title - 10
 	 */
-	// do_action( 'woocommerce_shop_loop_item_title' );
+	do_action( 'woocommerce_shop_loop_item_title' );
 
 	/**
 	 * Hook: woocommerce_after_shop_loop_item_title.

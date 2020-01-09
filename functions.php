fea6ac00399e213bb195ed8601b7d217a88dbbdb
Jack36VRN <jack3@JACK36-LAPTOP>
2020-01-09 11:32:56 +0300

    Update 2020/01/09 11:32 +3GMT
    Установлена тестовая кнопка "В корзину"

--- functions.php
+++ functions.php
@@ -62,6 +62,8 @@ add_action('woocommerce_before_shop_loop_item_title', 'woo_product_loop_thumbnai
 remove_action('woocommerce_shop_loop_item_title', 'woocommerce_template_loop_product_title', 10);
 add_action('woocommerce_shop_loop_item_title', 'woo_product_loop_title', 10);
 
+remove_action('woocommerce_after_shop_loop_item', 'woocommerce_template_loop_product_link_close', 5);
+
 
 
 //Новые функции для Хуков
--- woocommerce/content-product.php
+++ woocommerce/content-product.php
@@ -72,10 +72,9 @@ if ( empty( $product ) || ! $product->is_visible() ) {
 	/**
 	 * Hook: woocommerce_after_shop_loop_item.
 	 *
-	 * @hooked woocommerce_template_loop_product_link_close - 5
 	 * @hooked woocommerce_template_loop_add_to_cart - 10
 	 */
-	// do_action( 'woocommerce_after_shop_loop_item' );
+	do_action( 'woocommerce_after_shop_loop_item' );
 	?>
 	<hr>
 	<div class="art"><p>Артикул <span><?php echo $product->get_sku(); ?></span></p></div>
